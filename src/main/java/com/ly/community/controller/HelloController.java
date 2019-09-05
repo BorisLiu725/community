c6be6a29f5ea314064f7efd0f1e4be60c270b484
BorisLiu725 <2502644175@qq.com>
2019-09-05 14:12:34 +0800

    add some bootstrap css

--- src/main/java/com/ly/community/controller/HelloController.java
+++ src/main/java/com/ly/community/controller/HelloController.java
@@ -1,10 +1,7 @@
 package com.ly.community.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by BorisLiu on 2019/9/5
@@ -12,10 +9,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name")String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 
 
